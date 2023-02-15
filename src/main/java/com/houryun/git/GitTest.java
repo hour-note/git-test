5dade0fae2c817ef1dbfb944e42f8b28fa1b174e
hour <2506512160@qq.com>
2023-02-15 15:34:23 +0800

    master commit test3

--- src/main/java/com/houryun/git/GitTest.java
+++ src/main/java/com/houryun/git/GitTest.java
@@ -10,6 +10,7 @@ public class GitTest {
         System.out.println("hot-fix test");
 
         System.out.println("小时网络科技！");
+        System.out.println("12345");
     }
 
     public void test() {
