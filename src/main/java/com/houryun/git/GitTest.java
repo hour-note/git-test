2810585cae04fad49f6813aeb06a298598804d02
hour <2506512160@qq.com>
2023-02-15 14:52:07 +0800

    second commit!

---
+++ src/main/java/com/houryun/git/GitTest.java
@@ -0,0 +1,10 @@
+package com.houryun.git;
+
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("hello git!");
+        System.out.println("hello git!");
+        System.out.println("hello git!");
+        System.out.println("hello git!");
+    }
+}
